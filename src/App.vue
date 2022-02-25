c540d48c85ea936094c618f7f8ce95e5d237c38a
Ruslan Demidov <demidov.ruslan@gmail.com>
2022-02-25 12:36:32 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SelectItems from './components/SelectItems.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SelectItems
   }
 }
 </script>
+
+<template>
+  <div id="app">
+    <SelectItems />
+</div>
+</template>
+
 
 <style>
 #app {
---
+++ src/components/SelectItems.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "SelectItems"
+}
+</script>
+
+<template>
+  <div />
+</template>
+
+
+<style>
+
+</style>
