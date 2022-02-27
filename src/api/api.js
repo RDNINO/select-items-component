4c35e66fa0a306fe5df2255857bec9637107243b
Ruslan Demidov <demidov.ruslan@gmail.com>
2022-02-27 21:19:18 +0300

    feat (api) Add api

--- src/api/api.js
+++ src/api/api.js
@@ -1,22 +1,7 @@
 export default class Api {
   constructor() {
-    // this.url =
-    //   "https://dadata.ru/api/suggest/name";
-    // this.token = "43b7e62f8b5e35438a657ce267979f961dcb9670";
     this.url = "https://jsonplaceholder.typicode.com/users";
   }
-
-  // getOptions() {
-  //   return {
-  //     method: "POST",
-  //     mode: "cors",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "Authorization": `Token ${ this.token }`,
-  //       "Accept": "application/json",
-  //     },
-  //   };
-  // }
 
   getOptions() {
     return {
@@ -32,22 +17,14 @@ export default class Api {
     const options = this.getOptions();
     const request = {
       options,
-      // body: JSON.stringify({ query: queryString }),
     };
     try {
       const fetchRes = await fetch(this.url, request);
-      const response = await fetchRes.json()
-      const itemsData = response.map(item => item.name)
-      return itemsData
-      
+      const response = await fetchRes.json();
+      const itemsData = response.map((item) => item.name);
+      return itemsData;
     } catch (error) {
-      throw new Error(error)
+      throw new Error(error);
     }
-
-    // .then((res) => res.json())
-    // .then((items) => {
-    //   return items
-    //   // return items.map((element) => element.name);
-    // })
   }
 }
